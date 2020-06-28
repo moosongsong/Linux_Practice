a11efc9719ac6f29f0bab65562b692d6c1913569
moosong <songe08@gmail.com>
2020-06-27 21:04:08 -0700

    update

--- 0626/8_read_write.c
+++ 0626/8_read_write.c
@@ -3,6 +3,7 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
 
 //read
 //return : a number of bytes which is read
@@ -11,10 +12,43 @@ ssize_t readn(int fd, void *buf, size_t count){
         return -1;
     }
 
+    int totalRead = 0;
+    char * ptr = (char * )buf;
+
+    while (totalRead < count){
+        int nRead = read(fd, ptr, count - totalRead);
+
+        if (nRead < 0){
+            return -1;
+        } else if (nRead == 0){
+            return totalRead;
+        }
+        totalRead += nRead;
+        ptr += nRead;
+    }
+    return totalRead;
 }
 
 //write
 //return : a number of bytes which is written
 ssize_t writen(int fd, const void * buf, size_t count){
-    
+    if (fd < 0 || buf == NULL || count <= 0){
+        return -1;
+    }
+
+    int totalWrite = 0;
+    const char * ptr = (const char * )buf;
+
+    while (totalWrite < count){
+        int nWrite = read(fd, ptr, count - totalWrite);
+
+        if ( nWrite < 0){
+            return -1;
+        } else if ( nWrite == 0){
+            return totalWrite;
+        }
+        totalWrite +=  nWrite;
+        ptr +=  nWrite;
+    }
+    return totalWrite;
 }
